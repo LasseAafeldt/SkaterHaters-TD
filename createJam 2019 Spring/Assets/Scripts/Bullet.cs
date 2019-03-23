847d0a95bbf912e7e9aaf3820fc42b7516fb17b7
LasseAafeldt <superlla@hotmail.com>
2019-03-23 15:40:15 +0100

    base structure for tower shooting works as soon as enemies are set up properly.

---
+++ createJam 2019 Spring/Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+    public GameObject hitEffect;
+
+    public void setTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void FixedUpdate()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceToMoveThisFRame = speed * Time.deltaTime;
+
+        if(direction.magnitude < distanceToMoveThisFRame)
+        {
+            hitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceToMoveThisFRame, Space.World);
+    }
+
+    void hitTarget()
+    {
+        Debug.Log("The bullet hit something");
+        if(hitEffect != null)
+        {
+            GameObject effectInstance = (GameObject)Instantiate(hitEffect, transform.position, transform.rotation);
+            Destroy(effectInstance, 4f);
+        }
+        Destroy(gameObject);
+
+    }
+}
---
+++ createJam 2019 Spring/Assets/Scripts/Tower.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    private Transform target;
+
+    [Header("Attributes")]
+    public float range = 10f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+    public string enemyTag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        if(fireCountdown <= 0f)
+        {
+            shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void shoot()
+    {
+        Debug.Log(gameObject.name + " is shooting");
+        GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+        if(bullet != null)
+        {
+            bullet.setTarget(target);
+        }
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestEnemyDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(enemy.transform.position, transform.position);
+            if(distanceToEnemy < shortestEnemyDistance)
+            {
+                shortestEnemyDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestEnemyDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
