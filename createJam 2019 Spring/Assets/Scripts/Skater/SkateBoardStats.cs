fcfb8365f80584561f5ac5791eaa0c5894717529
Lasse Aafeldt <superlla@hotmail.com>
2019-06-16 22:11:53 +0200

    skater now picks up skateboard and rides it when he reches it.

---
+++ createJam 2019 Spring/Assets/Scripts/Skater/SkateBoardStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class SkateBoardStats : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        disableAgent();
+    }
+
+    public void setDestination(Vector3 destination)
+    {
+        agent.SetDestination(destination);
+        Debug.Log("Skateboard destination was set");
+    }
+
+    public void disableAgent()
+    {
+        agent.enabled = false;
+    }
+
+    public void enableAgent()
+    {
+        agent.enabled = true;
+    }
+}
--- createJam 2019 Spring/Assets/Scripts/Skater/SkaterMovement.cs
+++ createJam 2019 Spring/Assets/Scripts/Skater/SkaterMovement.cs
@@ -1,48 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class SkaterMovement : MonoBehaviour {
 
-    Transform sBoard;               
-    Transform start;
-    bool skateboard = false;
+    public Transform sBoard;
+    public Transform skaterFeet;
+    public Transform start;
     public float goalDistanceThreshold;
-    UnityEngine.AI.NavMeshAgent nav;               
+    UnityEngine.AI.NavMeshAgent nav;
+
+    bool skateboard;
+    public static bool aSkaterHasSkateboard;
+    public delegate void SkateboardPickupDelegate();
+    public event SkateboardPickupDelegate skateboardPickupEvent;
 
 
     void Awake() {
-        try
-        {
-            sBoard = GameObject.FindGameObjectWithTag("Skateboard").transform;
-            start = GameObject.FindGameObjectWithTag("Start").transform;
-        }
-        catch { }
-
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
+    private void Start()
+    {
+        skateboard = false;
+        aSkaterHasSkateboard = false;
+    }
 
     void Update() {
-        if (skateboard == false) {
-            GetSkateboard();
-        } else {
-            GotSkateboard();
+        //this is temp
+        UpdateMovementGoal();
+    }
+
+    void UpdateMovementGoal() {
+        if (!skateboard)
+        {
+            Vector3 skateboardXY = new Vector3(sBoard.position.x, transform.position.y, sBoard.position.z);
+            nav.SetDestination(skateboardXY);
+        }
+        else
+        {
+            Vector3 spawnXY = new Vector3(start.position.x, transform.position.y, start.position.z);
+            //        nav.SetDestination(start.position);
+            //check if goal is reached
+
+            if (Vector3.Distance(nav.transform.position, spawnXY) < goalDistanceThreshold)
+            {
+
+                //skateboard = false;
+                transform.SetParent(null);
+                //nav.enabled = true;
+
+                GameOver();
+            }
+        }
+        /*if (Vector3.Distance(nav.transform.position, sBoard.position) < goalDistanceThreshold) {
+            skateboard = true;
+        }*/
+    }
+
+    //check if skater runs into skateboard
+    private void OnTriggerEnter(Collider other)
+    {
+        if (aSkaterHasSkateboard || other.gameObject.layer != LayerMask.NameToLayer("Skateboard"))
+            return;
+        //Debug.Log("collision with: " + other.name);
+
+        //skater picks up skateboard
+        OnSkateboardPickup();
+    }
+
+    void OnSkateboardPickup()
+    {
+        skateboard = true;
+        // calleing out that the skateboard has been picked up
+        if (skateboardPickupEvent != null)
+            skateboardPickupEvent();
+
+        //attach the skatebaord to the skater who picked it up
+        sBoard.position = skaterFeet.position;
+        sBoard.rotation = transform.rotation;
+        sBoard.Rotate(skaterFeet.up, 90f);
+        transform.SetParent(sBoard);
+
+        //a skater has picked up the skateboard
+        aSkaterHasSkateboard = true;
+
+        // set destination for skateboard
+        SkateBoardStats sBoardStats = sBoard.GetComponent<SkateBoardStats>();
+        if(sBoardStats != null)
+        {
+            //disable skater nav agent
+            nav.enabled = false;
+            //enable the nav agent for skateboard
+            sBoardStats.enableAgent();
+            sBoardStats.setDestination(start.position);//new Vector3(start.position.x, transform.position.y, start.position.z));
+        }
+        else
+        {
+            Debug.LogError("Skateboard object has not SkateBoardStats component!");
         }
     }
 
-    void GetSkateboard() {
-        nav.SetDestination(sBoard.position);
-        if (Vector3.Distance(nav.transform.position, sBoard.position) < goalDistanceThreshold) {
-            skateboard = true;
-        }
-    }
-    
-    void GotSkateboard() {
-        nav.SetDestination(start.position);
-        
-        if (Vector3.Distance(nav.transform.position, start.position) < goalDistanceThreshold) {
-            skateboard = false;
-        }
-        
+    void GameOver()
+    {
+        Debug.Log("Player has lost the game... The skaters got their skateboard back!");
+        //make delegate and handle in gameManager script or something
     }
 }
