6522ec831902244073e0cb8a80194682f4fcdd4e
LasseAafeldt <superlla@hotmail.com>
2019-10-18 15:26:31 +0200

    Node editor window now has somewhat working zoom

--- createJam 2019 Spring/Assets/Editor/NodeBasedEditor.cs
+++ createJam 2019 Spring/Assets/Editor/NodeBasedEditor.cs
@@ -33,12 +33,84 @@ public class NodeBasedEditor : EditorWindow
     //private TowerNode towerNode;
     string[] workingFolders = { "Assets/ScriptableObjects/TowerBluprints" };
 
+    private const float kMinZoom = 0.1f;
+    private const float kMaxZoom = 5f;
+    private Rect _zoomArea = new Rect(0.0f, 75.0f, 600.0f, 300.0f - 100.0f);
+    private float _zoom = 1.0f;
+    private Vector2 _zoomCoordsOrigin = Vector2.zero;
+
     [MenuItem("Window/Node Based Editor")]
     private static void OpenWindow()
     {
         NodeBasedEditor window = GetWindow<NodeBasedEditor>();
         window.titleContent = new GUIContent("Node Based Editor");
+        window.minSize = new Vector2(800f, 600f);
+        window.wantsMouseMove = true;
+        window.Show();
     }
+
+    private Vector2 ConvertScreenCoordsToZoomCoords(Vector2 screenCoords)
+    {
+        NodeBasedEditor window = GetWindow<NodeBasedEditor>();
+        _zoomArea = window.position;
+        _zoomArea.x = 0f; _zoomArea.y = 0f;
+        return (screenCoords - _zoomArea.TopLeft()) / _zoom + _zoomCoordsOrigin;
+    }
+    private void DrawZoomArea()
+    {
+        // Within the zoom area all coordinates are relative to the top left corner of the zoom area
+        // with the width and height being scaled versions of the original/unzoomed area's width and height.
+        NodeBasedEditor window = GetWindow<NodeBasedEditor>();
+        _zoomArea = window.position;
+        _zoomArea.x = 0f; _zoomArea.y = 0f;
+        EditorZoomArea.Begin(_zoom, _zoomArea);
+
+        GUI.DrawTexture(new Rect(0, 0, maxSize.x, maxSize.y), backgroundTex, ScaleMode.StretchToFill); //draw background
+
+        DrawGrid(20, 0.2f, smallGridColor);
+        DrawGrid(100, 0.4f, bigGridColor);
+
+        DrawNodes();
+        DrawConnections();
+
+        DrawConnectionLine(Event.current);
+
+        EditorZoomArea.End();
+    }
+    private void HandleZoomEvents()
+    {
+        // Allow adjusting the zoom with the mouse wheel as well. In this case, use the mouse coordinates
+        // as the zoom center instead of the top left corner of the zoom area. This is achieved by
+        // maintaining an origin that is used as offset when drawing any GUI elements in the zoom area.
+        if (Event.current.type == EventType.ScrollWheel)
+        {
+            Vector2 screenCoordsMousePos = Event.current.mousePosition;
+            Vector2 delta = Event.current.delta;
+            Vector2 zoomCoordsMousePos = ConvertScreenCoordsToZoomCoords(screenCoordsMousePos);
+            float zoomDelta = -delta.y / 150.0f; //assume this is scrollwheel zoom speed
+            float oldZoom = _zoom;
+            _zoom += zoomDelta;
+            _zoom = Mathf.Clamp(_zoom, kMinZoom, kMaxZoom);
+            _zoomCoordsOrigin += (zoomCoordsMousePos - _zoomCoordsOrigin) - (oldZoom / _zoom) * 
+                (zoomCoordsMousePos - _zoomCoordsOrigin);
+
+            Event.current.Use();
+        }
+
+        // Allow moving the zoom area's origin by dragging with the middle mouse button or dragging
+        // with the left mouse button with Alt pressed.
+        if (Event.current.type == EventType.MouseDrag &&
+            (Event.current.button == 0 && Event.current.modifiers == EventModifiers.Alt) ||
+            Event.current.button == 2)
+        {
+            Vector2 delta = Event.current.delta;
+            delta /= _zoom;
+            _zoomCoordsOrigin += delta;
+
+            Event.current.Use();
+        }
+    }
+
 
     private void OnEnable()
     {
@@ -82,16 +154,10 @@ public class NodeBasedEditor : EditorWindow
 
     private void OnGUI()
     {
-        GUI.DrawTexture(new Rect(0, 0, maxSize.x, maxSize.y), backgroundTex, ScaleMode.StretchToFill); //draw background
-
-        DrawGrid(20, 0.2f, smallGridColor);
-        DrawGrid(100, 0.4f, bigGridColor);
-
-        DrawNodes();
-        DrawConnections();
-
-        DrawConnectionLine(Event.current);
-
+        HandleZoomEvents();
+        DrawZoomArea();        
+        
+        //Non zoomed stuff
         ProcessNodeEvents(Event.current);
         ProcessEvents(Event.current);
 
